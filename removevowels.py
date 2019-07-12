97ebb88ef431b2cc3116d10359894a49190ac232
sridevishanmugam <47391922+sridevishanmugam@users.noreply.github.com>
2019-07-12 18:07:37 +0530

    Update removevowels.py

--- removevowels.py
+++ removevowels.py
@@ -1,9 +1,13 @@
-def remove(string):
+def remove(n):
+    string=input()
+    string=string[0:n]
     vowels=('a','e','i','o','u')
     for x in string.lower():
         if x in vowels:
             string=string.replace(x, "")
     string=string[::-1]
     print(string)
-s=input()
-remove(s)
+    
+n=int(input())
+
+remove(n)
