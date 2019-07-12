2d152d06ad802fa368e750b25287dea94c417acf
sridevishanmugam <47391922+sridevishanmugam@users.noreply.github.com>
2019-07-12 17:33:09 +0530

    Create removevowels.py

---
+++ removevowels.py
@@ -0,0 +1,9 @@
+def remove(string):
+    vowels=('a','e','i','o','u')
+    for x in string.lower():
+        if x in vowels:
+            string=string.replace(x, "")
+            string=string[::-1]
+    print(string)
+s=input()
+remove(s)
