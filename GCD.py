cc99e1edad6f1b0610aebe38962b9d24464e59fa
sridevishanmugam <47391922+sridevishanmugam@users.noreply.github.com>
2019-07-13 16:08:51 +0530

    GCD.py

---
+++ GCD.py
@@ -0,0 +1,11 @@
+import math
+p1,q1=map(int,input().split())
+r1=[]
+s1=list(map(int,input().split()))
+for i in range(0,q1):
+    u1,v1=map(int,input().split())
+    r1.append([u1,v1])
+for i in r1:
+    x1=i[0]-1
+    y1=i[1]-1
+    print(math.gcd(s[x1],s[y1]))
