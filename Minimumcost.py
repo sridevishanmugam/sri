cd34df05f5005e52249c495f80a0bd7ae9f6e90d
sridevishanmugam <47391922+sridevishanmugam@users.noreply.github.com>
2019-07-13 15:25:37 +0530

    Update Minimumcost.py

--- Minimumcost.py
+++ Minimumcost.py
@@ -1,7 +1,7 @@
 a1,s1=input().split()
 s2=abs(len(s1)-len(a1))
 for i in range(len(a1)):
-  if(len(s)==1 and s1[i] in a1):
+  if(len(s1)==1 and s1[i] in a1):
     break
   if(a1[i]!=s1[i]):
     s2=s2+1
